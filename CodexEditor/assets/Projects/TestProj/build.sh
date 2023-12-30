b0eba1a57f9a40325f72cb66a088347dc496fc63
NeddX <nedbeargt@gmail.com>
2023-12-30 23:26:24 +0400

    Easier development

---
+++ CodexEditor/assets/Projects/TestProj/build.sh
@@ -0,0 +1,47 @@
+clear
+echo ":: Building 'Codex Template Project' (Debug)..."
+
+if [ -z "$CODEX_DEV" ]; then
+    echo ":: Codex development envrionment variables are not exported."
+    echo ":: Run 'build.sh' using source or the dot space operator if on bash."
+    echo ":: E.g.: source build.sh"
+    exit -1
+fi
+
+dependencies_binaries=("libCodexEngine-static.a" "libMGL.a" "libfmtd.a")
+
+if [ ! -d "lib" ]; then
+    mkdir lib
+fi
+if [ ! -d "builds" ]; then
+    mkdir -p builds/"$CODEX_CONFIG"
+fi
+if [ ! -d "builds/$CODEX_CONFIG" ]; then
+    mkdir builds/"$CODEX_CONFIG"
+fi
+
+echo ""
+echo ":: Copying neccessary dependencies..."
+for f in "${dependencies_binaries[@]}"; do
+    fp="$CODEX_BUILD_DIR/Codex/$f"
+    cp "$fp" ./
+    echo ":: Copied $fp"
+done
+
+echo ""
+echo ":: Running CMake"
+echo ""
+cmake ./ -G "$CODEX_CMAKE_GENERATOR" -B "builds/$CODEX_CONFIG"
+if [ ! $? -eq 0 ]; then
+    echo ":: CMake build generation failed. Build aborted."
+    exit -2
+fi
+
+cmake --build "builds/$CODEX_CONFIG" --parallel
+if [ ! $? -eq 0 ]; then
+    echo ":: CMake compilation failed. Build aborted."
+    exit -3
+fi
+
+echo ""
+echo ":: Build finished"
--- build.sh
+++ build.sh
@@ -11,7 +11,8 @@ arch=$(uname -m | tr '[:upper:]' '[:lower:]')
 platform="$kernel-$arch"
 build_conf="Debug"
 build_dir="$platform-$build_conf"
-build_dir="${build_dir,,}"
+build_dir=$(echo "${build_dir}" | tr '[:upper:]' '[:lower:]')
+cmake_build_generator="Unix Makefiles"
 
 if [ -z "$1" ]; then
     build_conf="Debug"
@@ -21,7 +22,8 @@ elif [[ "$1" == "clean" ]]; then
     fi
 elif [[ "$1" == "vs2022" ]]; then
 	mkdir -p "builds/vs2022"
-	cmake ./ -G "Visual Studio 17" -B "builds/vs2022"
+    cmake_build_generator="Visual Studio 17"
+	cmake ./ -G "$cmake_build_generator" -B "builds/vs2022"
 	cmake --build "builds/vs2022" --parallel
 	exit
 else
@@ -31,26 +33,27 @@ else
 fi
 
 clear
-echo "Build started"
-echo "OS: $kernel"
-echo "Architecture: $arch"
-echo "Configuration: $build_conf"
-echo "Build directory: $build_dir"
-echo "C Compiler: $CC"
-echo "CXX Compiler: $CXX"
+echo ":: Current working directory: $PWD"
+echo ":: Build started"
+echo ":: OS: $kernel"
+echo ":: Architecture: $arch"
+echo ":: Configuration: $build_conf"
+echo ":: Build directory: $build_dir"
+echo ":: C Compiler: $CC"
+echo ":: CXX Compiler: $CXX"
 echo ""
 
 if [ ! -d "builds" ]; then
-    echo "Configuring CMake..."
+    echo ":: Configuring CMake..."
     mkdir -p "builds/$build_dir"
 elif [ ! -d "builds/$build_dir" ]; then
-    echo "Configuring CMake..."
+    echo ":: Configuring CMake..."
     mkdir -p "builds/$build_dir"
 fi
 
-cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE="$build_conf" -B "builds/$build_dir"
+cmake -G "$cmake_build_generator" -DCMAKE_BUILD_TYPE="$build_conf" -B "builds/$build_dir"
 
-echo "Building..."
+echo ":: Building..."
 cmake --build "builds/$build_dir" --parallel
 cp "builds/$build_dir/compile_commands.json" ./
 
@@ -58,4 +61,12 @@ if [ ! -d "./bin" ]; then
     mkdir -p "./bin"
 fi
 
-echo "Build finished."
+# Export few envrionment variables
+echo ":: Exported Codex development envrionment variables." 
+export CODEX_DEV="yes"
+export CODEX_BUILD_GENERATOR="$cmake_build_generator"
+export CODEX_BUILD_DIR="$PWD"/builds/$build_dir
+export CODEX_ROOT="$PWD"
+export CODEX_CONFIG="$build_dir"
+
+echo ":: Build finished."
