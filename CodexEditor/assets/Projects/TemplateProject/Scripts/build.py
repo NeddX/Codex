2865391bf29b5907a83c361a4eed1a84ec6fe5b9
NeddX <nedbeargt@gmail.com>
2024-11-15 00:26:43 +0400

    Working on the build system to make it compatible with DAP

--- CodexEditor/assets/Projects/TemplateProject/Scripts/build.py
+++ CodexEditor/assets/Projects/TemplateProject/Scripts/build.py
@@ -8,6 +8,8 @@ import shutil
 import platform
 from enum import Enum
 import com
+import json
+
 
 class Action:
     Nope    = 0,
@@ -15,37 +17,71 @@ class Action:
     Clear   = 2,
     Install = 3
 
+
 action = Action.Build
 auxiliary_action = Action.Nope
 preset = None
+config = None
 stdoutput = None
 parallel = True
 run = False
+build_path = None
+cmake_ins = None
+launch_data = {
+    "version": "0.2.0",
+    "configurations": [
+        {
+            "name": "(LLDB Debug): Codex Editor",
+            "type": "lldb",
+            "request": "launch",
+            "program": None,
+            "cwd": None,
+            "args": [],
+        }
+    ],
+}
+
+
+def gen_launch_file():
+    cmake_ins = com.CMakeInspector(os.path.join("./builds/", preset))
+
+    install_dir = cmake_ins.get_var("CMAKE_INSTALL_PREFIX")
+    launch_data["configurations"][0]["program"] = os.path.join(
+        install_dir, "CodexEditor"
+    )
+    launch_data["configurations"][0]["cwd"] = install_dir
+
+    with open(".vscode/launch.json", "w+") as f:
+        json.dump(launch_data, f, indent=4)
+
 
 args = sys.argv[1:]
 for i in range(0, len(args)):
     arg = args[i]
     larg = arg.lower()
-    if larg == '--list':
-        com.log('Listing available presets')
+    if larg == "--list":
+        com.log("Listing available presets")
         presets = com.get_cmake_presets()
         for preset in presets:
             print(preset)
         action = Action.Nope
-    elif larg == '--build':
+    elif larg == "--build":
         action = Action.Build
-    elif larg.startswith('--preset='):
+    elif larg.startswith("--preset="):
         action = Action.Build
-        preset = arg.split('=')[1]
-    elif larg == '--no-out':
+        preset = arg.split("=")[1]
+    elif larg.startswith("--config"):
+        action = Action.Build
+        config = arg.split("=")[1]
+    elif larg == "--no-out":
         stdoutput = sb.DEVNULL
-    elif larg == '--no-parallel':
+    elif larg == "--no-parallel":
         parallel = False
-    elif larg == '--clear':
+    elif larg == "--clear":
         action = Action.Clear
-    elif larg == '--install':
+    elif larg == "--install":
         auxiliary_action = Action.Install
-    elif larg == '--run':
+    elif larg == "--run":
         run = True
 
 if action == Action.Build:
@@ -59,42 +95,67 @@ if action == Action.Build:
 
         presets = com.get_cmake_presets()
         if not presets:
-            com.panic(f'Platform not supported.')
+            com.panic("Platform not supported.")
 
-        if possible_builds:
-            common_el = set(possible_builds).intersection(presets)
+        if config == None:
+            if possible_builds:
+                common_el = set(possible_builds).intersection(presets)
 
-        if common_el:
-            preset = list(common_el)[0] # LOL
-            cmake_gen = False # Assume the user has already generated the build files.
-            com.log(f'Building: {preset}')
+            if common_el:
+                preset = list(common_el)[0]  # LOL
+                config = preset.split("-")[2]
+                cmake_gen = (
+                    False  # Assume the user has already generated the build files.
+                )
+                com.log(f"Building: {preset}")
+            else:
+                com.log(f"No preset provided, defaulting to: {presets[0]}")
+                preset = presets[0]
+                config = preset.split("-")[2]
         else:
-            com.log(f'Defaulting to: {presets[0]}')
-            preset = presets[0]
+            if possible_builds:
+                common_el = {s for s in possible_builds if config in s}
+
+            if common_el:
+                preset = list(common_el)[0]
+                cmake_gen = False
+                com.log(f"Building: {preset}")
+            else:
+                common_el = {s for s in presets if config in s}
+
+                if common_el:
+                    preset = list(common_el)[0]
+                    cmake_gen = True
+                    com.log(f"Building: {preset}")
+                else:
+                    com.panic("Configuration does not exist.")
 
     com.Chrono.begin()
-    com.log('CMake generation started.')
-    res = com.run(f'cmake --preset={preset}', stdout=stdoutput, stderr=stdoutput)
+    com.log("CMake generation started.")
+    res = com.run(f"cmake --preset={preset}", stdout=stdoutput, stderr=stdoutput)
     if res.returncode != 0:
         if not preset in com.get_cmake_presets():
-            com.panic(f'Build failed because \'{preset}\' is not an actual preset.')
+            com.panic(f"Build failed because '{preset}' is not an actual preset.")
         else:
-            com.panic('CMake generation failed for unknown reason(s).')
+            com.panic("CMake generation failed for unknown reason(s).")
 
     elapsed = com.Chrono.end()
-    com.log('CMake generation finished. Took: ' + '{:.2f}ms'.format(elapsed))
+    com.log("CMake generation finished. Took: " + "{:.2f}ms".format(elapsed))
 
     com.Chrono.begin()
-    res = com.run(f'cmake --build builds/{preset}' + (' --parallel 8' if parallel else ''), stdout=stdoutput)
+    res = com.run(
+        f"cmake --build builds/{preset}" + (" --parallel 8" if parallel else ""),
+        stdout=stdoutput,
+    )
     if res.returncode != 0:
-        com.panic('Build failed for unknown reason(s).')
+        com.panic("Build failed for unknown reason(s).")
 
     elapsed = com.Chrono.end()
-    com.log(f'CMake build finished. Took: ' + '{:.2f}ms'.format(elapsed))
+    com.log(f"CMake build finished. Took: " + "{:.2f}ms".format(elapsed))
 
     #  Might write a separate script for installation.
     if auxiliary_action == Action.Install:
-        com.log('CMake installation started.')
+        com.log("CMake installation started.")
         com.Chrono.begin()
 
         if platform.system() in ['Linux', 'Darwin']:
@@ -104,26 +165,27 @@ if action == Action.Build:
             #    res = com.run(f'cmake --install builds{preset}', stdout=stdoutput)
             #else:
             #    com.win32_request_admin()
-            #    com.panic("Don't worry, this is not a real panic.")
-            com.run(f'cmake --install builds/{preset}', stdout=stdoutput)
+            #    com.panic("Don"t worry, this is not a real panic.")
+            com.run(f"cmake --install builds/{preset}", stdout=stdoutput)
 
         if res.returncode != 0:
-            com.panic('CMake install unexpectedly failed.')
+            com.panic("CMake install unexpectedly failed.")
 
         elapsed = com.Chrono.end()
-        com.log(f'CMake installation finished. Took: ' + '{:.2f}ms'.format(elapsed))
+        gen_launch_file()
+        com.log(f"CMake installation finished. Took: " + "{:.2f}ms".format(elapsed))
 
 elif action == Action.Clear:
     if preset == None:
         presets = com.get_cmake_presets()
         if not presets:
-            com.panic(f'Platform not supported.')
-        com.log(f'Defaulting to: {presets[0]}')
+            com.panic(f"Platform not supported.")
+        com.log(f"Defaulting to: {presets[0]}")
         preset = presets[0]
 
     com.Chrono.begin()
-    com.run(f'cmake --build builds/{preset} --target clean')
+    com.run(f"cmake --build builds/{preset} --target clean")
     elapsed = com.Chrono.end()
-    com.log(f'CMake clean finished. Took: ' + '{:.2f}ms'.format(elapsed))
+    com.log(f"CMake clean finished. Took: " + "{:.2f}ms".format(elapsed))
 
-com.log(f'Done.')
+com.log(f"Done.")
--- CodexEditor/assets/Projects/TemplateProject/Scripts/com.py
+++ CodexEditor/assets/Projects/TemplateProject/Scripts/com.py
@@ -9,9 +9,12 @@ import ctypes
 import platform
 from enum import Enum
 import subprocess as sb
+from datetime import datetime
+
 
 class Chrono:
     tp = None
+
     @staticmethod
     def begin():
         Chrono.tp = time.time()
@@ -19,42 +22,73 @@ class Chrono:
     @staticmethod
     def end():
         if Chrono.tp is None:
-            raise ValueError('Called Chrono.end() before calling Chrono.begin()')
+            raise ValueError("Called Chrono.end() before calling Chrono.begin()")
         elapsed = time.time() - Chrono.tp
         tp = None
         return elapsed * 1000
 
+
+class CMakeInspector:
+    cache_file = None
+
+    def __init__(self, build_dir):
+        self.cache_file = os.path.join(build_dir, "CMakeCache.txt")
+        if not os.path.exists(self.cache_file):
+            raise RuntimeError(f"Invalid build directory.")
+
+    def get_var(self, var_name):
+        with open(self.cache_file, "r") as fs:
+            for line in fs:
+                if line.startswith(var_name):
+                    return line.strip().split("=")[1]
+
+
 def log(msg):
-    print(f'[Build] [Info] :: {msg}')
+    print(f'[{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}] [Info] (Build) :: {msg}')
+
 
 def warn(msg):
-    print(f'[Build] [Warning] :: {msg}')
+    print(
+        f'[{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}] [Warning] (Build) :: {msg}'
+    )
+
 
 def err(msg):
-    print(f'[Build] [Panic] :: {msg}')
+    print(f'[{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}] [Panic] (Build) :: {msg}')
 
-def panic(msg, exit_code = 1):
+
+def panic(msg, exit_code=1):
     err(msg)
     sys.exit(exit_code)
 
-def run(cmd, shell = True, stdout = None, stderr = None, capture_output = False, text = None):
-    return sb.run(cmd, shell=shell, stdout=stdout, stderr=stderr, capture_output=capture_output, text=text)
+
+def run(cmd, shell=True, stdout=None, stderr=None, capture_output=False, text=None):
+    return sb.run(
+        cmd,
+        shell=shell,
+        stdout=stdout,
+        stderr=stderr,
+        capture_output=capture_output,
+        text=text,
+    )
+
 
 def get_cmake_presets():
-    res = run('cmake --list-presets', shell=True, stdout=sb.PIPE)
+    res = run("cmake --list-presets", shell=True, stdout=sb.PIPE)
     if res:
-        output = res.stdout.decode('utf-8')
-        if output == '':
+        output = res.stdout.decode("utf-8")
+        if output == "":
             return []
     else:
         return []
 
     # Pythonic autism
-    presets = output.replace(' ', '').split('\n')
+    presets = output.replace(" ", "").split("\n")
     presets = presets[2:-1]
-    presets = [(s[:s.rfind('"')])[1:] for s in presets]
+    presets = [(s[: s.rfind('"')])[1:] for s in presets]
 
     return presets
+
 
 def win32_is_admin():
     try:
@@ -62,5 +96,8 @@ def win32_is_admin():
     except:
         return False
 
+
 def win32_request_admin():
-    ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
+    ctypes.windll.shell32.ShellExecuteW(
+        None, "runas", sys.executable, " ".join(sys.argv), None, 1
+    )
